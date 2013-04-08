748b4096c63e3e3674ec2107f824502508074073
Alex Hortin <hortinstein@gmail.com>
2013-04-08 09:21:45 -0400

    added methods into index.js, need to move some basic error checking here

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
 //modular for future expansion to other APIs
 var api = require('./middleware/digital_ocean/digital_ocean.js');
+
+
+//these bind the primary functions to their API.  This is not 100% nessecary, but done for visibility and in case of future expansion.
+//general error checking should be moved here eventually
+var swarm_init = function(input_json, callback) {
+    api.swarm_init(input_json,callback);
+};
+
+var curator_scale = function(curator_id, size, callback) {
+    api.curator_scale(curator_id,size, callback);
+};
+
+var trove_scale = function(trove_ids, trove_size, trove_number, callback) {
+    api.trove_size(trove_ids,trove_size,trove_number,callback);
+};
+
+var swarmite_reset = function(ids, callback) {
+    api.swarmite_reset(ids,callback);
+};
+
+var destroy_swarm = function(ids, callback) {
+    api.destroy_swarm(ids,callback);
+};
+
+
+
+//if the module is executed directly
+if (!module.parent) {
+    console.log("swarmlicator");
+    var argv = require('optimist').argv;
+    
+} else { //being called as a module, expose the API
+
+    var swarmlicator = {}; 
+    swarmlicator.swarm_init = swarm_init;
+    swarmlicator.curator_scale = curator_scale;
+    swarmlicator.trove_scale = trove_scale;
+    swarmlicator.swarmite_reset = swarmite_reset;
+    swarmlicator.destroy_swarm = destroy_swarm;
+
+}
--- middleware/digital_ocean/digital_ocean.js
+++ middleware/digital_ocean/digital_ocean.js
@@ -1,32 +1,37 @@
-
 var digital_ocean = require('brinydeep');
 
-var swarm_init = function (input_json, callback){
-	try {
-		var api_key = input_json.provider_info.api_info.api_key;
-		var client_key = input_json.provider_info.api_info.client_key;
-		var vm_image = input_json.provider_info.vm_image_id;
-		var region_id = input_json.provider_info.region_id;
-		var curator_size = input_json.schematic.curator_size;
-		var trove_size = input_json.schematic.trove_size;
-		
+var api = {};
 
-
-	} catch(err){
-		callback(err,null);
-	}
+api.swarm_init = function(input_json, callback) {
+    try {
+        var api_key = input_json.provider_info.api_info.api_key;
+        var client_key = input_json.provider_info.api_info.client_key;
+        var vm_image = input_json.provider_info.vm_image_id;
+        var region_id = input_json.provider_info.region_id;
+        var curator_size = input_json.schematic.curator_size;
+        var trove_size = input_json.schematic.trove_size;
+    }
+    catch (err) {
+        callback(err, null);
+    }
 
 
 };
-var curator_scale = function (curator_id, size, callback){
+
+api.curator_scale = function(curator_id, size, callback) {
 
 };
-var trove_scale = function (trove_ids, trove_size, trove_number, callback){
+
+api.trove_scale = function(trove_ids, trove_size, trove_number, callback) {
 
 };
-var swarmite_reset = function (ids, callback){
-    
-}
-var destroy_swarm = function (ids, callback){
-    
-}
+
+api.swarmite_reset = function(ids, callback) {
+
+};
+
+api.destroy_swarm = function(ids, callback) {
+
+};
+
+module.exports = api;
